14d84ee58abc6d25d0f084dda662a1b89a3bc160
aafmisra <aafmisra@gmail.com>
2020-01-07 15:26:16 -0500

    getting organized
    project is currently in silver state, I added comments to js file to explain each function, I strategized for quiz mode, which will make it gold

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-//create event listener to flip and back forth between card front and back
-// const flipButton = document.querySelector(".flip-card");
-
-// flipButton.addEventListener("click", flipCard)
-
-// function flipCard() {
-//     if (cardFront.style.display === "inline-block" && cardBack.style.display === "none") {
-//         cardFront.style.display = "none"
-//         cardBack.style.display = "inline-block"
-//     } else {
-//         cardFront.style.display = "inline-block"
-//         cardBack.style.display = "none"
-//     }
-// }
-
 //get a list of all rhode island animals of a certain class from gbif
 //creat arrays of the different taxas
 //deduplicate the lists
-//return new deduped lists
-
-// const mammalID = 359
-// const birdID = 212
-// const amphibianID = 131
-// const reptileID = 358
+//return new deduped list
 
 function getSpecies(classID) {
   return fetch(
@@ -47,7 +27,7 @@ function getSpecies(classID) {
     });
 }
 
-getSpecies(classID).then(r => console.log(r));
+//take the speciesKey and return an object with taxa names
 
 function getNames(speciesKey) {
   return fetch(`http://api.gbif.org/v1/species/${speciesKey}`)
@@ -65,20 +45,8 @@ function getNames(speciesKey) {
     });
 }
 
-getNames(5231190).then(r => console.log(r));
 
-// function getPictureURL(speciesKey) {
-//   return fetch(
-//     `http://api.gbif.org/v1/occurrence/search?speciesKey=${speciesKey}&mediaType=StillImage`
-//   )
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(response) {
-//       return response.results[0].media[0].identifier;
-//     });
-// }
-
+//search eol API using species name from gbif API and return eol species id
 function getEOLspeciesID(speciesName) {
     return fetch(`https://eol.org/api/search/1.0.json?q=${speciesName}&page=1&key=`)
         .then(function (response) {
@@ -88,8 +56,9 @@ function getEOLspeciesID(speciesName) {
             return response.results[0].id;
         });
 }
-getEOLspeciesID("Mephitis mephitis").then(r => console.log(r))
 
+
+//take eol species id and return url of a picture from species page
 function getEOLPictureURL(eolID) {
     return fetch(`https://eol.org/api/pages/1.0/${eolID}.json?details=true&images_per_page=1`)
         .then(function (response) {
@@ -100,16 +69,7 @@ function getEOLPictureURL(eolID) {
         });
 }
 
-getEOLspeciesID("Mephitis mephitis").then(getEOLPictureURL).then(r => console.log(r))
-
-
-
-//URL to use if I want click event on cards to go to eol page for that animal: https://eol.org/api/search/1.0.json?q=Mephitis%2Bmephitis&exact=true&page=1&key=
-//change species name to speciesName ie in getEOLPictureuRL: let speciesInfo = getInfo(speciesKey)
-// speciesName = speciesInfo.species
-// return fetch(`${speciesName}`)
-//should probably return link from first object in the array (most likely to exactly match)
-
+//combine taxa names and picture url into one object
 function getInfo(speciesKey) {
   let namesPromise = getNames(speciesKey);
 
@@ -120,6 +80,13 @@ function getInfo(speciesKey) {
     });
   });
 }
+
+//create a div element representing a card
+//create a dl element for the card
+//create dt elements and append labels from getInfo to them
+//create dd elements and append names from getInfo to them
+//create an img element for the card
+//append url from getInfo as src attribute of img
 
 function getInfoList(info) {
   const namesList = document.createElement('dl');
@@ -170,12 +137,12 @@ function getInfoList(info) {
   animalPhoto.setAttribute('src', info.picture);
   animalPhoto.setAttribute('width', '150px');
   cardFront.style.backgroundImage = `url(${info.picture})`
-  cardFront.style.backgroundSize = "cover"
   card.appendChild(cardFront);
 
   return card;
 }
 
+//loop that uses helper functions and array of species to create cards
 cardGallery = document.querySelector('.gallery');
 getSpecies(classID).then(function(speciesKeys) {
   for (var speciesKey of speciesKeys) {
@@ -186,11 +153,106 @@ getSpecies(classID).then(function(speciesKeys) {
   }
 });
 
-function getImageElement(info) {}
+//event listener for shuffle button
+const shuffleButton = document.querySelector(".shuffle-cards");
 
-//create a div element representing a card
-//create a dl element for the card
-//create dt elements and append labels from getInfo to them
-//create dd elements and append names from getInfo to them
-//create an img element for the card
-//append url from getInfo as src attribute of img
+shuffleButton.addEventListener("click", function(){
+    location.reload();
+})
+
+
+//URL to use if I want click event on cards to go to eol page for that animal: https://eol.org/api/search/1.0.json?q=Mephitis%2Bmephitis&exact=true&page=1&key=
+//change species name to speciesName ie in getEOLPictureuRL: let speciesInfo = getInfo(speciesKey)
+// speciesName = speciesInfo.species
+// return fetch(`${speciesName}`)
+//should probably return link from first object in the array (most likely to exactly match)
+
+
+
+
+
+
+
+
+
+
+
+//QUIZ MODE
+//click "im ready for a quiz button"
+//when button is clicked, dd elements on the cards change to input fields
+//text content of button changes to "i need to study"
+//user types answers in the input fields
+//if all input.value match thecorresponding info.{nametype}, then the card disappears
+//if user clicks "i need to study" button, cards switch back to the default state and button changes back to "im ready for a quiz"
+
+
+
+// cardGallery = document.querySelector('.gallery');
+// getSpecies(classID).then(function (speciesKeys) {
+//     for (var speciesKey of speciesKeys) {
+//         getInfo(speciesKey).then(function (info) {
+//             let card = getInfoList(info);
+//             cardGallery.appendChild(card);
+//         });
+//     }
+// });
+
+// function getInfoList(info) {
+//     const namesList = document.createElement('dl');
+//     namesList.setAttribute("class", "card-names")
+
+//     const classType = document.createElement('dt');
+//     classType.innerText = 'Class';
+//     const className = document.createElement('dd');
+//     className.innerText = info.Class;
+//     namesList.append(classType);
+//     namesList.append(className);
+
+//     const orderType = document.createElement('dt');
+//     orderType.innerText = 'Order';
+//     const orderName = document.createElement('dd');
+//     orderName.innerText = info.Order;
+//     namesList.append(orderType);
+//     namesList.append(orderName);
+
+//     const familyType = document.createElement('dt');
+//     familyType.innerText = 'Family';
+//     const familyName = document.createElement('dd');
+//     familyName.innerText = info.Family;
+//     namesList.append(familyType);
+//     namesList.append(familyName);
+
+//     const genusType = document.createElement('dt');
+//     genusType.innerText = 'Genus';
+//     const genusName = document.createElement('dd');
+//     genusName.innerText = info.Genus;
+//     namesList.append(genusType);
+//     namesList.append(genusName);
+
+//     const speciesType = document.createElement('dt');
+//     speciesType.innerText = 'Species';
+//     const speciesName = document.createElement('dd');
+//     speciesName.innerText = info.Species;
+//     namesList.append(speciesType);
+//     namesList.append(speciesName);
+
+//     let card = document.createElement('div');
+//     card.appendChild(namesList);
+//     card.setAttribute('class', 'gallery-card');
+
+//     const animalPhoto = document.createElement('img');
+//     const cardFront = document.createElement('div');
+//     cardFront.setAttribute("class", "card-photo")
+//     animalPhoto.setAttribute('src', info.picture);
+//     animalPhoto.setAttribute('width', '150px');
+//     cardFront.style.backgroundImage = `url(${info.picture})`
+//     card.appendChild(cardFront);
+
+//     return card;
+// }
+
+const quizButton = document.querySelector(".quiz-button");
+quizButton.addEventListener("click", switchToQuizMode);
+
+function switchToQuizMode(){}
+
